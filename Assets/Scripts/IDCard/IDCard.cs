effd65da21a1f1e4cd2db832bd9798714211413a
Maty83 <matyashalir@gmail.com>
2023-11-25 18:50:09 +0100

    Add random ID generator function.

--- Assets/Scripts/IDCard/IDCard.cs
+++ Assets/Scripts/IDCard/IDCard.cs
@@ -31,6 +31,37 @@ public class IDCard : ScriptableObject
     public string TownOfResidence = "Chicago";
     public MaritalStatus maritalStatus = MaritalStatus.single;
 
+    public static IDCard generateNewRandomInstance()
+    {
+        IDCard instance = new();
+        var names=NameLists.GetNames();
+        var surnames=NameLists.GetSurnames();
+        var cities=NameLists.GetCities();
+        instance.FirstName = names[Random.Range(0, names.Length)];
+        instance.Surname = surnames[Random.Range(0, surnames.Length)];
+        instance.age = (uint)Random.Range(21, 51);
+        instance.TownOfOrigin = cities[Random.Range(0, cities.Length)];
+        instance.TownOfResidence = cities[Random.Range (0, cities.Length)];
+        int randstate = Random.Range(0, 10);
+        if(randstate < 3)
+        {
+            instance.maritalStatus= MaritalStatus.single;
+        }
+        else if(randstate < 5) 
+        {
+            instance.maritalStatus = MaritalStatus.married;
+        }
+        else if(randstate < 8)
+        {
+            instance.maritalStatus = MaritalStatus.divorced;
+        }
+        else
+        {
+            instance.maritalStatus = MaritalStatus.widower;
+        }
+        return instance;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
